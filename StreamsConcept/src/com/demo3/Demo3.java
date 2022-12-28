01786d71c1f206f86556007ec5cc5e9daafa3dcf
Jarvis <rohitchavanrc7@gmail.com>
2022-12-28 10:47:57 +0530

    modification

--- StreamsConcept/src/com/demo3/Demo3.java
+++ StreamsConcept/src/com/demo3/Demo3.java
@@ -1,10 +1,10 @@
-package com.demo2;
+package com.demo3;
 
-public class Demo2 {
-	
+public class Demo3 {
+
 	public static void main(String[] args) {
 
-		System.out.println("Hello Demo2");
+		System.out.println("Hello Demo3");
 
 		// New joinee making some changes
 		// Making some more changes
